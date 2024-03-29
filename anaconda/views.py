5a2136a259689f4a22a1479be64a955b1657c872
Rob Smith <sleepingkingstudios@gmail.com>
2024-03-29 16:02:02 -0400

    Add minimal layout.

--- anaconda/views.py
+++ anaconda/views.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from anaconda import application
 
 
 @application.route('/')
-def hello_world():
-    return '<p>Greetings, starfighter!</p>'
+def index():
+    return render_template('index.html');
