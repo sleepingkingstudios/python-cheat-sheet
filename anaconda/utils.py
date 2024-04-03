ada116f08d8379f9931025df2fd8e3a8549a8ab8
Rob Smith <sleepingkingstudios@gmail.com>
2024-04-03 12:26:49 -0400

    Auto-generate heading IDs.
    - Add beautifulsoup4 dependency.

--- anaconda/utils.py
+++ anaconda/utils.py
@@ -1,7 +1,61 @@
+from re import split, sub
 from typing import Any
 from flask import render_template
 from markdown import markdown
 from markupsafe import Markup
+from bs4 import BeautifulSoup
+
+
+__HEADING_TAGS__ = ['h1', 'h2', 'h3', 'h4', 'h5', 'g6']
+
+
+def __kebab_case_word__(word: str) -> str:
+    normal = sub(r'[^\w]+', '', word)
+    chars = (f'-{char.lower()}' if char.isupper() else char for char in normal)
+
+    return sub(r'^-', '', ''.join(chars))
+
+
+def kebab_case(string: str) -> str:
+    """
+    Converts a string to kebab-case.
+
+    First, separates the string into words. Then, for each word:
+
+    - Normalizes the word by removing non-letter, non-digit characters.
+    - Replaces any uppercase characters with the lowercase letter
+      preceded by a dash.
+    - Trims a leading dash, if any.
+
+    Finally, joins the words with dash characters.
+
+    Arguments:
+        string (str): The input to process.
+
+    Returns:
+        str: The input string in kebab-case.
+    """
+    words = split(r'[ \-_]', string)
+    words = (__kebab_case_word__(word) for word in words)
+
+    return '-'.join(words)
+
+
+def __add_header_ids__(fragment: BeautifulSoup) -> BeautifulSoup:
+    """
+    Adds auto-generated id attributes to header tags.
+
+    Arguments:
+        fragment (BeautifulSoup): The HTML to process.
+
+    Returns:
+        BeautifulSoup: The processed HTML.
+    """
+    for heading_tag in __HEADING_TAGS__:
+        for tag in fragment.find_all(heading_tag):
+            tag.attrs['id'] = __kebab_case_word__(tag.text)
+
+    return fragment
 
 
 def render_markdown(template_name: str, **context: Any) -> Markup:
@@ -17,5 +71,7 @@ def render_markdown(template_name: str, **context: Any) -> Markup:
     """
     raw_text = render_template(template_name, **context)
     rendered = markdown(raw_text)
+    fragment = BeautifulSoup(rendered, features="html.parser")
+    fragment = __add_header_ids__(fragment)
 
-    return Markup(rendered)
+    return Markup(fragment)
--- anaconda/utils_test.py
+++ anaconda/utils_test.py
@@ -2,15 +2,39 @@ import pytest
 from inspect import cleandoc
 from jinja2 import TemplateNotFound
 from markupsafe import Markup
+from bs4 import BeautifulSoup
 
 from anaconda import application
-from anaconda.utils import render_markdown
+from anaconda.utils import kebab_case, render_markdown
 
 
 @pytest.fixture
 def with_app_context():
     with application.app_context():
         yield
+
+
+class TestKebabCase:
+    def test_empty_string(self):
+        assert kebab_case('') == ''
+
+    def test_lowercase_string(self):
+        assert kebab_case('lowercase') == 'lowercase'
+
+    def test_capitalized_string(self):
+        assert kebab_case('Capitalized') == 'capitalized'
+
+    def test_camel_case_string(self):
+        assert kebab_case('CamelCase') == 'camel-case'
+
+    def test_kebab_case_string(self):
+        assert kebab_case('kebab-case') == 'kebab-case'
+
+    def test_snake_case_string(self):
+        assert kebab_case('snake_case') == 'snake-case'
+
+    def test_string_with_punctuation(self):
+        assert kebab_case('Greetings, Programs!') == 'greetings-programs'
 
 
 class TestRenderMarkdown:
@@ -31,7 +55,7 @@ class TestRenderMarkdown:
         )
         expected = cleandoc(
             """
-            <h1>Greetings, Starfighter!</h1>
+            <h1 id="greetings-starfighter">Greetings, Starfighter!</h1>
             <p>You have been recruited by the Star League to defend the
             frontier against Xur and the Ko-Dan Armada.</p>
             """
